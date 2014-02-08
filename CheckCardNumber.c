018eda658886ef0a4b7b1660bf49162e3c13dd0c
Anton Vynogradenko <restuta8@gmail.com>
2014-02-08 15:46:17 -0800

    Update CheckCardNumber.c
    Code review

--- CheckCardNumber.c
+++ CheckCardNumber.c
@@ -7,28 +7,31 @@ int main(void)
 {
     printf("Give me your credit card number: ");
     long long cardNumber = GetLongLong();
+    //review: rename to cardNumberLength
     int lengthNumber = floor(log10(cardNumber));// length of card number without 1 digit.
-    long long power = pow (10, lengthNumber);
+    long long power = pow (10, lengthNumber); //review: rename this one also e.g. currentDivider or so
     
-    int sumDigits = 0;
+    int sumDigits = 0; //review: rename to digitsSum 
     int firstDigit = 0;
     int secondDigit = 0;
-    int i = 0;
-    long long curdNumberLeft = 0;
-    string typeCard = "INVALID";
+    int i = 0; //review: move this variable inside for loop
+    long long curdNumberLeft = 0; //review: cardDigitsLeft (curd -> card) and add comment explaining
+    string typeCard = "INVALID"; //review: mb. cardType ?
     
     //check the sum of all digits by Luhn’s algorithm - it's more then nessesary in my task, but let it be.
     
     curdNumberLeft = cardNumber; //restuta is cool, love him - I love him already )))
-    for (i = 0; i <= (lengthNumber); i += 2)
+    for (i = 0; i <= lengthNumber; i += 2)
     {
-        firstDigit = curdNumberLeft/power;
+        firstDigit = curdNumberLeft / power;
         curdNumberLeft = curdNumberLeft - firstDigit * power;
         power = power/10;
+        
         if (power >= 1)
         {
-            secondDigit = curdNumberLeft/power;
+            secondDigit = curdNumberLeft / power;
             curdNumberLeft = curdNumberLeft - secondDigit * power;
+            
             if (i == 0)
             {
                 if (firstDigit == 3 && (secondDigit == 4 || secondDigit == 7))
@@ -38,19 +41,22 @@ int main(void)
                 else if (firstDigit == 4)
                     typeCard = "VISA";
             }
-            secondDigit = secondDigit * 2;
-            power = power/10;
+            secondDigit = secondDigit * 2; //review: can be rewritten as secondDigit *= 2;
+            power = power / 10;
+            
             if (secondDigit > 9)
+                //review: explain what is going on here in comments
                 secondDigit = secondDigit / 10 + secondDigit - (secondDigit / 10) * 10; //ask Restuta! stupid girl!!!       
         }
-        else
+        else 
             secondDigit = 0;
+            
         sumDigits = sumDigits + firstDigit + secondDigit;
     }
+    
     // I need it for checking Luhn’s algorithm only
     //if (sumDigits % 10 > 0)
       // typeCard = "INVALID";
 
     printf("%s\n", typeCard);
-    
 }
